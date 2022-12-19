2ca7da722c1fb29763860e6764f75438e3b5d381
Kianeay <70838054+Kianeay@users.noreply.github.com>
2022-12-19 18:30:37 +0300

    feat: add  work finished message

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-import { argv } from 'node:process';
+import { argv, exit } from 'node:process';
+import { stdin as input, stdout as output } from 'node:process';
+import readline from 'node:readline';
+
+const rl = readline.createInterface({ input, output });
 
 const userName = argv.slice(2)[0]
   ? argv.slice(2)[0].split('=').slice(1)[0]
@@ -8,3 +12,10 @@ if (!userName) {
   console.log(`please, write your --username`);
   process.exit(0);
 } else console.log(`Welcome to the File Manager, ${userName}!`);
+
+rl.on('line', (line) => {
+  console.log(line);
+}).on('close', () => {
+  console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
+  exit();
+});
